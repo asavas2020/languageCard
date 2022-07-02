7c82ad37cc1fa8c16ede406ee790ed0c26d0388e
Gokhan <gknsvsdata@gmail.com>
2022-07-02 22:24:29 +0300

    event

--- src/components/item/Item.js
+++ src/components/item/Item.js
@@ -1,13 +1,24 @@
+import { useState } from "react";
 import "./Item.css";
 
 const Item = ({name, img, options}) => {
+    const [showLogo, setShowLogo] = useState(true);
     //  const [name, img, options] = item
   return (
     <div className="card">
+      { showLogo ? (<div>  
         <img className="card-logo" src={img} alt="image" />
         <h3 className="card-title">{name}</h3>
+      </div> )
+       : (<ul>
+        { options.map((element, index) => {
+            return <li key = {index}>{element}</li>;
+          })}
+       </ul>) }
+      
+      
     </div>
-  )
-}
+  );
+};
 
 export default Item
